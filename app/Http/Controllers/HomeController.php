720282d4235c94456ed994862fbc2cb32896b677
Reza Hamidpour <hamidpourr@gmail.com>
2023-09-16 16:45:20 -0700

    Preparing about_us section for demo.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,12 +10,12 @@ class HomeController extends Controller
 {
    public function index(){
 
-       $about_us = ['title' => "SADRA RESTURANT" , "body" => "At Sadra, we care about your health and ours. Our staff maintain the highest levels of personal hygiene and we make sure the equipment are being sanitized carefully and vigorously. Your safety and peace of mind matters the most when we serve you.A restaurant is a business that connects people through delicious food, comfortable space, and memorable service. We believe that the greatest restaurants not only connect people, but also build lasting relationships, both staff-to-guest and staff-to-staff."];
+       $about_us = ['title' => "SADRA RESTURANT" , "body" =>
+           "At Sadra, we care about your health and ours. Our staff maintain the highest levels of personal hygiene and we make sure the equipment are being sanitized carefully and vigorously. Your safety and peace of mind matters the most when we serve you.A restaurant is a business that connects people through delicious food, comfortable space, and memorable service. We believe that the greatest restaurants not only connect people, but also build lasting relationships, both staff-to-guest and staff-to-staff.",
+           "since" => '2002'];
 
         $popular_items = $this->popular_items();
         $gallery = $this->getGallery();
-
-
 
        return view('client-side.index', compact('about_us', 'popular_items', 'gallery'));
 
--- resources/views/client-side/sections/about-us-section.blade.php
+++ resources/views/client-side/sections/about-us-section.blade.php
@@ -9,22 +9,27 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-6">
-                <div class="AboutUsContent">
-                    <h2>SADRA RESTURANT</h2>
-                    <p>At Sadra, we care about your health and ours. Our staff maintain the highest levels of personal hygiene and we make sure the equipment are being sanitized carefully and vigorously. Your safety and peace of mind matters the most when we serve you.</p>
-                    <p>A restaurant is a business that connects people through delicious food, comfortable space, and memorable service. We believe that the greatest restaurants not only connect people, but also build lasting relationships, both staff-to-guest and staff-to-staff.</p>
-                    <div class="AboutUsContentSignature">
-                        <img src="{{ asset('client-side/dist/images/logo-handwrite.png') }}" class="img-fluid" alt="">
+            @if( isset($about_us))
+                <div class="col-lg-6">
+                    <div class="AboutUsContent">
+                        <h2>{{ $about_us['title'] }}</h2>
+                        <p>{{ $about_us['body'] }}</p>
+                        <div class="AboutUsContentSignature">
+                            <img src="{{ asset('client-side/dist/images/logo-handwrite.png') }}" class="img-fluid"
+                                 alt="">
+                        </div>
+
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="AboutUsImage">
-                    <div class="AboutUsSince"><span><i>since</i><i>2002</i></span></div>
-                    <img src="{{ asset('client-side/dist/images/mario-mesaglio-7BZzlV0Z9R4-unsplash-780x520.jpg') }}" class="img-fluid" alt="">
+                <div class="col-lg-6">
+                    <div class="AboutUsImage">
+                        <div class="AboutUsSince"><span><i>since</i><i>{{ $about_us['since'] }}</i></span></div>
+                        <img
+                            src="{{ asset('client-side/dist/images/mario-mesaglio-7BZzlV0Z9R4-unsplash-780x520.jpg') }}"
+                            class="img-fluid" alt="">
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 </div>
