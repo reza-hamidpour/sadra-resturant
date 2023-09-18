0ff5079c326a6de71566679fd1e8af17bfd1e338
Reza Hamidpour <hamidpourr@gmail.com>
2023-09-18 14:04:13 -0700

    Adding Gallery admin panel routes.

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::prefix('/admin')->middleware(['web', 'auth'])->namespace('admin')->group(
     Route::get('/dashboard', function () {
         return view('Admin.index');
     })->name('dashboard');
+
     Route::get("/foods", [FoodsController::class, 'index'])->name('foods-index');
     Route::get("/foods/create", [FoodsController::class, 'create'])->name("food-create");
     Route::post('/foods/create', [FoodsController::class, 'store'])->name('food-store');
@@ -40,6 +41,14 @@ Route::prefix('/admin')->middleware(['web', 'auth'])->namespace('admin')->group(
     Route::get('/foods_type/update/{food_type}', [FoodsTypeController::class, 'show'])->name('foods_type_edit');
     Route::patch('/foods_type/update/{food_type}', [FoodsTypeController::class, 'update'])->name('foods_type_update');
     Route::get('/foods_type/delete/{food_type}', [FoodsTypeController::class, 'destroy'])->name('foods_type_destroy');
+
+    Route::get('/gallery', [\App\Http\Controllers\Admin\GalleryController::class, 'index'])->name('gallery-index');
+    Route::get('/gallery/create', [\App\Http\Controllers\Admin\GalleryController::class, 'create'])->name('gallery-create');
+    Route::post('/gallery/create', [\App\Http\Controllers\Admin\GalleryController::class, 'store'])->name('gallery-store');
+    Route::get('/gallery/{gallery}/edit', [\App\Http\Controllers\Admin\GalleryController::class, 'edit'])->name('gallery-edit');
+    Route::patch('/gallery/{gallery}/edit', [\App\Http\Controllers\Admin\GalleryController::class,'update'])->name('gallery-update');
+    Route::delete('/gallery/{gallery}/delete', [\App\Http\Controllers\Admin\GalleryController::class, 'destroy'])->name('gallery-destroy');
+
 
 });
 
