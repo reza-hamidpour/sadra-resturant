ea49b1bfcd5c6289ac17de3f5c9e0441f9f6ec36
Reza Hamidpour <hamidpourr@gmail.com>
2023-09-26 19:23:48 -0700

    Implementing loader method 'index' and just passing necessary variables.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Foods_type;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    //
+    public function index(Request $request){
+        $page_background = asset('client-side/dist/images/bg-menu-page.jpg');
+        $page_title = "Restaurant menu";
+        $page_description = "The list of the best dishes in Sadra Restaurant.";
+        $categories = Foods_type::all();
+
+        return view('client-side.pages.menu', compact('categories', 'page_background', 'page_title', 'page_description'));
+    }
 }
