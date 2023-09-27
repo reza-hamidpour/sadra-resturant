62d2b7910be732391d6ca0c0117cbbde048f81a6
Reza Hamidpour <hamidpourr@gmail.com>
2023-09-26 19:22:55 -0700

    Bugfix on pages-slider

---
+++ resources/views/client-side/layouts/pages-slider.blade.php
@@ -0,0 +1,10 @@
+<div class="page-head" style="background-image: url('{{ $page_background }}');">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>{{ $page_title }}</h2>
+                <p>{{ $page_description }}</p>
+            </div>
+        </div>
+    </div>
+</div>
