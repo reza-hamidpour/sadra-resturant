3964dfbfa2920b4bf968f0ec550dc13b1a2df8ef
Reza Hamidpour <hamidpourr@gmail.com>
2023-10-13 12:56:32 -0700

    Implementing, adding new row for a option.

--- public/Admin/assets/js/admin-foods.js
+++ public/Admin/assets/js/admin-foods.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function(){
     jQuery(document).on('click',"#add_new_option", function(e) {
-        e.preventDefault();
+        e.preventDefault(); // Prevent default behaviour of buttons
+        // Copy our element and appending to its parent.
         var new_pic = $('.foods-options[data-option=temp-option]').clone(true);
         new_pic.appendTo("#wrapper-options");
         new_pic.slideDown();
@@ -18,15 +19,16 @@ jQuery(document).ready(function(){
             pre += 1;
         });
         pre = 0;
+        $("button.add_option_options").each(function(){
+            $(this).attr('data-target', 'option-' + pre);
+            $(this).attr('data-groupname', 'option_' + pre);
+            pre += 1;
+        })
+        pre = 0;
+        // Reparing options options parent fields
         $('div[data-optionparent]').each(function(){
            $(this).attr('data-optionparent', 'option-' + pre);
             let childNumber = 0;
-           $(this).find('input[data-optiontype=name]').each(function(){
-               $(this).attr('name', 'option_name[' + pre + "][" + childNumber + "]");
-               $(this).attr('data-optionoption', 'option_' + pre);
-               childNumber += 1;
-           });
-           childNumber = 0;
             $(this).find('input[data-optiontype=value]').each(function(){
                 $(this).attr('name', 'option_value[' + pre + "][" + childNumber + "]");
                 $(this).attr('data-optionoption', 'option_' + pre);
@@ -61,7 +63,14 @@ jQuery(document).ready(function(){
                 $(this).attr('data-optionoption', 'option_' + optionParent_id );
             });
         }
-
+        jQuery(document).on('click', 'button.add_option_options', function(e){
+            e.preventDefault();
+            let target = $(this).attr('data-target');
+            let new_row = $('div.option_options[data-optionparent=temp-option] div.row:first-child').clone(true);
+            $('div.option_options[data-optionparent=' + target + ']').append(new_row);
+            new_row.appendTo('#wrapper-options');
+            new_row.slideDown(200);
+        });
     });
 
 });
