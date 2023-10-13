eaa5e3732a6ac54ee6a754f4945e08c273736dc5
Reza Hamidpour <hamidpourr@gmail.com>
2023-10-12 20:20:25 -0700

    Bugfix on menu UI.

--- resources/views/client-side/pages/menu.blade.php
+++ resources/views/client-side/pages/menu.blade.php
@@ -24,20 +24,19 @@
                              data-items="category-{{ $category->id }}">
                             @foreach($category->foods()->get() as $food)
                                 <div class="col-lg-6 col-12">
-                                    <div class="menu-products-item">
+                                    <a href="#" data-bs-toggle="modal" data-bs-target="#FoodOpttions-{{ $food->id }}" class="menu-products-item-href">
+                                        <div class="menu-products-item">
                                         <div class="menu-products-item-img">
-                                            <a href="#">
                                                 <img src="{{ $food->getThumbnail() }}" alt="{{ $food->title }}">
-                                            </a>
                                         </div>
                                         <div class="menu-products-item-text">
                                             <div class="menu-products-item-text-title">
-                                                <a href="#"><h2>{{ $food->title }}</h2></a>
+                                                <h2>{!! $food->title !!}</h2>
                                                 <p>{{ $food->ingredient }}</p>
                                             </div>
                                             <div class="menu-products-item-price">
                                                 <span>${{ $food->price }}</span>
-                                                <a href="#" data-bs-toggle="modal"
+                                                <button href="#" data-bs-toggle="modal"
                                                    data-bs-target="#FoodOpttions-{{ $food->id }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                                          width="24" height="24">
@@ -45,10 +44,11 @@
                                                             d="M15.3709 3.43994L18.5819 9.00194L22.0049 9.00212V11.0021L20.8379 11.0019L20.0813 20.0852C20.0381 20.6035 19.6048 21.0021 19.0847 21.0021H4.92502C4.40493 21.0021 3.97166 20.6035 3.92847 20.0852L3.17088 11.0019L2.00488 11.0021V9.00212L5.42688 9.00194L8.63886 3.43994L10.3709 4.43994L7.73688 9.00194H16.2719L13.6389 4.43994L15.3709 3.43994ZM18.8309 11.0019L5.17788 11.0021L5.84488 19.0021H18.1639L18.8309 11.0019ZM13.0049 13.0021V17.0021H11.0049V13.0021H13.0049ZM9.00488 13.0021V17.0021H7.00488V13.0021H9.00488ZM17.0049 13.0021V17.0021H15.0049V13.0021H17.0049Z"
                                                             fill="currentColor"></path>
                                                     </svg>
-                                                </a>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                                 <!-- Modal Select Purchase options -->
                                 <div class="modal fade" id="FoodOpttions-{{ $food->id }}" tabindex="-1"
@@ -57,7 +57,7 @@
                                         class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-lg">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="FoodOpttionsLabel">{{ $food->title }}</h1>
+                                                <h1 class="modal-title fs-5" id="FoodOpttionsLabel">{!! $food->title !!}</h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                         aria-label="Close"></button>
                                             </div>
