35ea77c73ffdd92d40e329b008d91d0810d98b10
Reza Hamidpour <hamidpourr@gmail.com>
2023-10-22 19:59:44 -0700

    Adding index functionality to our controller.

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function index(){
+
+        return view('client-side.pages.reservation');
+    }
+
+
+    public function reservation(){
+
+    }
+
 }
